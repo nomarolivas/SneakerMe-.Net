ff47cae21b2450ec67cfac7acf2e4162136dc2cf
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2023-09-07 20:23:51 -0700

    Update NavMenu.js

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -81,25 +81,3 @@ export class NavMenu extends Component {
   }
 }
 
-
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-//     return (
-        
-//         <nav className="navbar">
-//             <h1 className='websiteTitle'>SneakerMe</h1>
-//             <div className="tabLinks">
-//                 <Link  className= "link" to="/">Home</Link>
-//                 <Link  className= "link" to="/Sneakers">Sneakers</Link>
-//                 <Link className= "link" to="/Sale">Sale</Link>
-//                 <Link className= "link" to="/About">About Us</Link>
-//                 <Link className= "link" to="/Login">Login</Link>
-
-//             </div>
-
-//         </nav>
-//     );
-// }
-
-// export default Navbar;
