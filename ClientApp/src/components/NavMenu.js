0b9e2bcc306bd1da276ab58a102d7ebeed5a8a51
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2023-09-07 20:22:55 -0700

    Navbar update

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -2,52 +2,81 @@ import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
-import './NavMenu.css';
+
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+  // constructor (props) {
+  //   super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+  //   this.toggleNavbar = this.toggleNavbar.bind(this);
+  //   this.state = {
+  //     collapsed: true
+  //   };
+  // }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+  // toggleNavbar () {
+  //   this.setState({
+  //     collapsed: !this.state.collapsed
+  //   });
+  // }
 
   render() {
     return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">sneakerMe</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
+      // <header>
+      //   <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+      //     <NavbarBrand tag={Link} to="/">sneakerMe</NavbarBrand>
+      //     <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+      //     <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+      //       <ul className="navbar-nav flex-grow">
+              // <NavItem>
+              //   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+              // </NavItem>
 
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
+              // <NavItem>
+              //   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+              // </NavItem>
 
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
+              // <NavItem>
+              //   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+              // </NavItem>
 
-              <LoginMenu>
-              </LoginMenu>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
+      //         <LoginMenu>
+      //         </LoginMenu>
+      //       </ul>
+      //     </Collapse>
+      //   </Navbar>
+      // </header>
+
+      <Navbar className="navbar">
+        <h1 className='websiteTitle'>SneakerMe</h1>
+        <div className='tabLinks'>
+        {/* <NavItem>
+          <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+        </NavItem> */}
+
+        
+
+          <Link className="link" to="/">Home</Link>
+          <Link className="link" to="/Sneakers">Sneakers</Link>
+          <Link className="link" to="/Sale">Sale</Link>
+          <Link className="link" to="/About">About Us</Link>
+          <Link className="link" to="/Login">Login</Link>
+       
+        </div>
+
+
+
+      </Navbar>
     );
   }
 }
