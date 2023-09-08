c24a6f2b3ebbf0cb87f7c070ab4f052e08324169
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2023-09-08 16:23:34 -0700

    Update NavMenu.js

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -24,30 +24,7 @@ export class NavMenu extends Component {
 
   render() {
     return (
-      // <header>
-      //   <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-      //     <NavbarBrand tag={Link} to="/">sneakerMe</NavbarBrand>
-      //     <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-      //     <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-      //       <ul className="navbar-nav flex-grow">
-      //         <NavItem>
-      //           <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-      //         </NavItem>
 
-      //         <NavItem>
-      //           <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-      //         </NavItem>
-
-      //         <NavItem>
-      //           <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-      //         </NavItem>
-
-      //         <LoginMenu>
-      //         </LoginMenu>
-      //       </ul>
-      //     </Collapse>
-      //   </Navbar>
-      // </header>
 
       <Navbar className="navbar">
         <h1 className='websiteTitle'>SneakerMe</h1>
